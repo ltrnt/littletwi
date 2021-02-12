e8f65871b38fa2a1fe93d90d9cc317c79f7a189c
Maxim Filippov <maxim.fil1905@yandex.ru>
2021-02-12 13:10:33 +0400

    Initial commit

---
+++ src/main/java/ru/ltrnt/littletwi/LittletwiApplication.java
@@ -0,0 +1,14 @@
+package ru.ltrnt.littletwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittletwiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittletwiApplication.class, args);
+    }
+
+}
